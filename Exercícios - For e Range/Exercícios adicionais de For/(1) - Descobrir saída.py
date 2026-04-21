6e1e373ad7c547ca03ddeab78f5fd750483c0f57
lucasdinizps4-sketch <lucasdiniz.ps4@gmail.com>
2026-04-21 18:58:56 +0000

    Exercício

---
+++ Exercícios - For e Range/Exercícios adicionais de For/(1) - Descobrir saída.py
@@ -0,0 +1,21 @@
+# Para o programa a seguir: 
+
+V1 = int(input("Digite o valor de V1: "))
+V2 = int(input("Digite o valor de V2: "))
+for I in range(1, 6, 2):
+    V1 = int(input("Digite o valor de V1: "))
+    V2 = int(input("Digite o valor de V2: "))
+    print("V1:", V1, "V2:", V2)
+
+
+# a) Quantos valores são lidos para V1?
+    # 4 valores (1 fora do loop e 3 dentro)
+
+# b) Quantos valores são lidos para V2?
+    # 4 valores (1 fora do loop e 3 dentro)
+
+# c) Quantas linhas são impressas?
+    # 3 linhas
+
+# d) Que valores I possuirá?
+    # 1, 3, 5
---
+++ Exercícios - For e Range/Exercícios adicionais de For/(2) - Descobrir saída com dados definidos.py
@@ -0,0 +1,19 @@
+# Considerando o programa do exercício anterior e supondo que o usuário irá utilizar os dados a seguir,
+    # 2
+    # 35
+    # 72
+    # 8
+    # 3
+    # 91
+    # 0
+    # 10
+
+# a) Quais os valores são lidos para V1, V2?
+    # V1: 2, 72, 3, 0
+    # V2: 35, 8, 91, 10
+
+ # # b) Quais os valores impressos?
+    # V1: 72, V2: 8
+    # V1: 3, V2: 91
+    # V1: 0, V2: 10
+
