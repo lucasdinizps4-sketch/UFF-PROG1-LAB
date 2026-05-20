c792015479ee287b0cf0dd75281e7eca55a8f8ad
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-05-20 16:33:44 -0300

    Exercício

--- Exercícios de Lab/Jogo da velha.py
+++ Exercícios de Lab/Jogo da velha.py
@@ -8,6 +8,7 @@ jogo = [
 ]
 casas_faltando = 0
 jogador = "X"
+venceu = False
 
 
 while casas_faltando < 9:
@@ -15,15 +16,19 @@ while casas_faltando < 9:
         for j in range(len(jogo[i])):
             print(f"{jogo[i][j]}", end=" | ")
         print("\n----------------")
-    if jogo[i][0] == jogo[i][1] and jogo[i][1] == jogo[i][2] and jogo[i][0] != " ":
-        print(f"O jogador {jogo[i][0]} ganhou na linha [i]")
+
+        if jogo[i][0] == jogo[i][1] and jogo[i][1] == jogo[i][2] and jogo[i][0] != " ":
+            venceu = True
+            print(f"\nO jogador {jogo[i][0]} ganhou na linha {[i]}\n")
+            break
+    if venceu == True:
         break
 
 
     print(f"\nVez do jogador {jogador}\n")
 
-    linha = int(input("Informe a linha: (0, 1 ou 2)"))
-    casa = int(input("Informe a casa: (0, 1 ou 2)"))
+    linha = int(input("\nInforme a linha: (0, 1 ou 2)"))
+    casa = int(input("\nInforme a casa: (0, 1 ou 2)"))
 
     if jogo[linha][casa] == " ":
         jogo[linha][casa] = jogador
