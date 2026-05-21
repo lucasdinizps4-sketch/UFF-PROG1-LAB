9b1ebc1d663ac12064f7c875a92cd5af11b05c20
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-05-21 00:46:39 -0300

    Exercício

--- Exercícios de Lab/Jogo da velha.py
+++ Exercícios de Lab/Jogo da velha.py
@@ -15,12 +15,20 @@ while casas_faltando < 9:
     for i in range(len(jogo)):
         for j in range(len(jogo[i])):
             print(f"{jogo[i][j]}", end=" | ")
-        print("\n----------------")
+        print("\n------------")
+
+    for i in range(len(jogo)):    
 
         if jogo[i][0] == jogo[i][1] and jogo[i][1] == jogo[i][2] and jogo[i][0] != " ":
             venceu = True
             print(f"\nO jogador {jogo[i][0]} ganhou na linha {[i]}\n")
-            break
+        elif jogo[0][i] == jogo[1][i] and jogo[1][i] == jogo[2][i] and jogo[0][i] != " ":
+            venceu = True
+            print(f"O jogador {jogo[0][i]} ganhou na coluna {i}")
+    if (jogo[0][0] == jogo[1][1] and jogo[1][1] == jogo[2][2] or jogo[0][2] == jogo[1][1] and jogo [1][1] == jogo[2][0]) and jogo[1][1] != " ":
+        venceu = True
+        print(f"O jogador {jogo[1][1]} venceu na diagonal")
+
     if venceu == True:
         break
 
@@ -40,6 +48,8 @@ while casas_faltando < 9:
     elif jogo[linha][casa] != " ":
         print("\n\nCasa está ocupada, tente dnv\n\n")
         continue
+
+print("\n\nDeu velha! Não houve vencedor")
 
 
     
