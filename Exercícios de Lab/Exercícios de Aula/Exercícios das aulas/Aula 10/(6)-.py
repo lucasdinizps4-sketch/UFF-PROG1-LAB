a88bf20a1c7e1901e72d25dc9366fedb6a2ddfd5
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-05-25 23:09:13 -0300

    Exercício corrigido

--- Exercícios de Lab/Exercícios de Aula/Exercícios das aulas/Aula 10/(6)-.py
+++ Exercícios de Lab/Exercícios de Aula/Exercícios das aulas/Aula 10/(6)-.py
@@ -1,4 +1,14 @@
-frase = input()
+alunos = {
+    "Lucas": [7,8,5],
+    "Marcos": [3,6,5]
+}
+
+for chave,dados in alunos.items():
+    soma = 0
+    for i in dados:
+        soma += i
+    media = soma/(len(dados))
+    print(f"Nome: {chave} \nMédia: {media:.2f}")
 
 
 
