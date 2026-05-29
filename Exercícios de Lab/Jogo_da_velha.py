5b388f1e7b36e6bf7fa9397c3725c3d52ab84f6c
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-05-29 18:39:02 -0300

    Adicionando legendas

--- Exercícios de Lab/Jogo_da_velha.py
+++ Exercícios de Lab/Jogo_da_velha.py
@@ -21,31 +21,31 @@ while casas_faltando < 9: # Criei um loop while com condição de casas no jogo,
 
         if jogo[i][0] == jogo[i][1] and jogo[i][1] == jogo[i][2] and jogo[i][0] != " ": # Condição de vitória na vertical, a linha sendo i vai sempre avanar até linha 2, depois de verificar todas casas. Ao final uma condição parar garantir que as primeiras não estejam vazias 
             venceu = True # Vencedor vira True
-            print(f"\nO jogador {jogo[i][0]} ganhou na linha {[i]}\n") # Não usei {jogador}, pois daria errado. Ele sempre printaria o jogador seguinte, pois houve a troca de variaveis antes de testar as condições 
+            print(f"\nO jogador {jogo[i][0]} ganhou na linha {[i]}\n") # Não usei {jogador}, pois daria errado. Ele sempre printaria o jogador seguinte, pois houve a troca de variaveis antes de testar as condições. Exemplo, caso X ganhasse, le imprimiria O pois houve uma troca
         elif jogo[0][i] == jogo[1][i] and jogo[1][i] == jogo[2][i] and jogo[0][i] != " ": # Inverti colocando i parar casa, desse modo consigo  
             venceu = True
-            print(f"O jogador {jogo[0][i]} ganhou na coluna {i}")
-    if (jogo[0][0] == jogo[1][1] and jogo[1][1] == jogo[2][2] or jogo[0][2] == jogo[1][1] and jogo [1][1] == jogo[2][0]) and jogo[1][1] != " ":
+            print(f"O jogador {jogo[0][i]} ganhou na coluna {i}") 
+    if (jogo[0][0] == jogo[1][1] and jogo[1][1] == jogo[2][2] or jogo[0][2] == jogo[1][1] and jogo [1][1] == jogo[2][0]) and jogo[1][1] != " ": # Fiz de forma manual, comparando as extremidades e o ponto central
         venceu = True
         print(f"O jogador {jogo[1][1]} venceu na diagonal")
 
-    if venceu == True:
+    if venceu == True: # Caso alguem vença ele quebra o programa
         break
 
 
-    print(f"\nVez do jogador {jogador}")
+    print(f"\nVez do jogador {jogador}") 
 
-    linha = int(input("\nInforme a linha: (0, 1 ou 2): "))
+    linha = int(input("\nInforme a linha: (0, 1 ou 2): ")) # Crio as variaveis de coordenada e peço um valor para elas
     casa = int(input("\nInforme a casa: (0, 1 ou 2): "))
 
-    if jogo[linha][casa] == " ":
-        jogo[linha][casa] = jogador
-        casas_faltando += 1
-        if jogador == "X":
+    if jogo[linha][casa] == " ": # Caso as coordenadas estejam vazias, adiciono valor de jogador nela
+        jogo[linha][casa] = jogador # Adicionando valor de jogador na coordenada escolhida
+        casas_faltando += 1 # Contador para sabermos quantas casas temos
+        if jogador == "X": # Troca de variaveis, começa sempre com X. Caso seja X, vira O. 
             jogador = "O"
         else:
-            jogador = "X"
-    elif jogo[linha][casa] != " ":
+            jogador = "X" # Caso seja O vira X após a jogada
+    elif jogo[linha][casa] != " ": # Caso as coordenadas não sejam vazias, printa uma mensagem de aviso e retorna para inicio
         print("\n\nCasa está ocupada, tente dnv\n\n")
         continue
 
@@ -55,5 +55,9 @@ if casas_faltando == 9:
         for j in range(len(jogo[i])): # for para percorrer as casas
             print(f"{jogo[i][j]}", end=" | ") # printa todas casas e divide com |
         print("\n------------")
+
+
+
+
 
     
