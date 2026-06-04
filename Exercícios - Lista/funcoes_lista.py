26ae2093b9d5acfc2fb09426c97bb0cef289ac03
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-06-04 20:26:27 -0300

    Adicionando os enunciados

--- Exercícios - Lista/funcoes_lista.py
+++ Exercícios - Lista/funcoes_lista.py
@@ -1,8 +1,12 @@
+
+# Escreva uma função para ler a quantidade valores inteiros que o usuário quiser e add em uma lista 
 def add_lista(lista,qnt):
     for _ in range(qnt):
         n = int(input("Informe valor de N: "))
         lista.append(n)
 
+
+# Escreva uma função para devolver a posição do menor valor inteiro em uma lista
 def pos_menor(lista):
     menor = lista[0]
     p_menor = 0
@@ -12,6 +16,8 @@ def pos_menor(lista):
             p_menor = i
     return p_menor
 
+
+# Escreva uma função para descobrir o menor valor inteiro em uma lista de apartir de uma posição
 def obtermenor_por_pos(lista,posinicial):
     menor = lista[posinicial]
     p_menor = posinicial
@@ -21,11 +27,15 @@ def obtermenor_por_pos(lista,posinicial):
             p_menor = i
     return p_menor
 
+
+# Escreva uma função para permutar 2 valores na lista de acordo com os indices passados
 def permutar(lista,p1,p2):
     aux = lista[p1]
     lista[p1] = lista[p2]
     lista[p2] = aux
 
+
+# Escreva uma função para ordenar uma lista
 def ordenar_lista(lista):
     for posinicial in range(len(lista)-1):
         posmenor = obtermenor_por_pos(lista,posinicial)
