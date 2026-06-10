2e7d74a16c1c9e7e8a15363a6e3c21e84f41bcb3
Lucas Diniz <lucasdiniz.ps4@gmail.com>
2026-06-09 22:47:11 -0300

    Consertando print

--- Exercícios de Lab/Exercício aulas/Exercícios de slides/Aula (8) -/(2) -.py
+++ Exercícios de Lab/Exercício aulas/Exercícios de slides/Aula (8) -/(2) -.py
@@ -7,14 +7,19 @@ nova_frase = ""
 if modo == "descriptografar":
     chave = chave *(-1)
 
-    for i in range(len(mensagem)):
-        caractere = mensagem[i].lower()
+for i in range(len(mensagem)):
+    caractere = mensagem[i].lower()
 
-        if caractere in alfabeto:
-            indice_atual = alfabeto.find(caractere)
-            novo_indice = (indice_atual + chave) % 26
-            nova_frase += alfabeto[novo_indice]
-        else:
-            nova_frase += mensagem[i]
+    if caractere in alfabeto:
+        indice_atual = alfabeto.find(caractere)
+        novo_indice = (indice_atual + chave) % 26
+        nova_frase += alfabeto[novo_indice]
+    else:
+        nova_frase += mensagem[i]
 
-print(f"Mensagem criptgrafada: {nova_frase}")
+if modo == "descriptografar":
+    print(f"Mensagem criptgrafada: {nova_frase}")
+
+else:
+    print(f"Mensagem descriptgrafada: {nova_frase}")
+
